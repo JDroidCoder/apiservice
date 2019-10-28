7309fc7e5494c4b4e4e563640d7673a07bae6751
AnonUnfeeling <anonunfeeling1@gmail.com>
2019-10-28 13:49:44 +0200

    s

--- apiservice/src/main/java/jdroidcoder/ua/apiservice/network/Base.kt
+++ apiservice/src/main/java/jdroidcoder/ua/apiservice/network/Base.kt
@@ -3,4 +3,4 @@ package jdroidcoder.ua.apiservice.network
 import com.google.gson.annotations.SerializedName
 
 class Base(@SerializedName("status") var status:Int,
-           @SerializedName("meesage")var message:String)
+           @SerializedName("message")var message:String)
